30601248dcd030bb60944efbc5f8323d90034be3
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-07-05 17:41:35 +0600

    comment

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.use(cors())
 app.use(express.json())
 
 
-
+// connect with mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gflzo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// token verify 
 function verifyJWT(req, res, next) {
     const authorization = req.headers.authorization;
     if (!authorization) {
@@ -36,10 +36,21 @@ async function run() {
         const serviceCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('booking');
         const userCollection = client.db('doctors_portal').collection('user');
+        const doctorsCollection = client.db('doctors_portal').collection('doctors');
+        const verifyAdmin = async (req, res, next) =>{
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if(requesterAccount.role === 'admin'){
+                next();
+            } else{
+                res.status(403).send({message: 'forbidden'});
 
+            }
+        }
         app.get('/services', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).project({name: 1});
             const services = await cursor.toArray();
             res.send(services)
         });
@@ -53,20 +64,14 @@ async function run() {
             const isAdmin = user.role === 'admin';
             res.send({admin: isAdmin})
         })
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
-            const requester = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({ email: requester });
-            if (requesterAccount.role === 'admin') { 
                 const filter = { email: email };
                 const updateDoc = {
                     $set: { role: 'admin' },
                 };
                 const result = await userCollection.updateOne(filter, updateDoc);
                 res.send(result);
-            } else{
-                res.status(403).send({message: 'forbidden'});
-            }
 
         })
 
@@ -83,11 +88,26 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ result, token })
         })
-
+        app.get('/doctors', verifyJWT, verifyAdmin, async(req, res) =>{
+            const doctors = await doctorsCollection.find().toArray();
+            res.send(doctors);
+          })
+        app.post('/doctors', verifyJWT, verifyAdmin,  async (req, res) =>{
+            const doctor = req.body;
+            const result = await doctorsCollection.insertOne(doctor);
+            res.send(result);
+        })
+        app.delete('/doctors/:email', verifyJWT, verifyAdmin,  async (req, res) =>{
+            const email = req.params.email;
+            const filter = {email: email}
+            const result = await doctorsCollection.deleteOne(filter);
+            res.send(result);
+        })
 
 
         // this is not the proper way to  query.
         //After learning more about mongodb . use aggrreegrate lookup pipeline match group
+
         app.get('/available', async (req, res) => {
             const data = req.query.data;
             //step 1: get all services
